5dd652abe131b0a2021e83760315fff32267e516
Theo <ZCN8899@GMAIL.COM>
2026-05-25 17:37:51 +0800

    Update nitro.config.ts

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,38 +1,38 @@
 import process from "node:process"
-import { join } from "node:path"
-import viteNitro from "vite-plugin-with-nitro"
-import { RollopGlob } from "./tools/rollup-glob"
-import { projectDir } from "./shared/dir"
+  import { join } from "node:path"
+  import viteNitro from "vite-plugin-with-nitro"
+  import { RollopGlob } from "./tools/rollup-glob"
+  import { projectDir } from "./shared/dir"
 
-const nitroOption: Parameters<typeof viteNitro>[0] = {
-  experimental: {
-    database: true,
-  },
-  rollupConfig: {
-    plugins: [RollopGlob()],
-  },
-  sourceMap: false,
-  database: {
-    default: {
-      connector: "better-sqlite3",
+  const nitroOption: Parameters<typeof viteNitro>[0] = {
+    experimental: {
+      database: true,
     },
-  },
-  devDatabase: {
-    default: {
-      connector: "better-sqlite3",
+    rollupConfig: {
+      plugins: [RollopGlob()],
     },
-  },
-  imports: {
-    dirs: ["server/utils", "shared"],
-  },
-  preset: "node-server",
-  alias: {
-    "@shared": join(projectDir, "shared"),
-    "#": join(projectDir, "server"),
-  },
-}
+    sourceMap: false,
+    database: {
+      default: {
+        connector: "better-sqlite3",
+      },
+    },
+    devDatabase: {
+      default: {
+        connector: "better-sqlite3",
+      },
+    },
+    imports: {
+      dirs: ["server/utils", "shared"],
+    },
+    preset: "node-server",
+    alias: {
+      "@shared": join(projectDir, "shared"),
+      "#": join(projectDir, "server"),
+    },
+  }
 
-if (process.env.VERCEL) {
+  if (process.env.VERCEL) {
     nitroOption.database = {
       default: {
         connector: 'sqlite',
@@ -42,31 +42,30 @@ if (process.env.VERCEL) {
         },
       },
     }
-  }
-} else if (process.env.CF_PAGES) {
-  nitroOption.preset = "cloudflare-pages"
-  nitroOption.unenv = {
-    alias: {
-      "safer-buffer": "node:buffer",
-    },
-  }
-  nitroOption.database = {
-    default: {
-      connector: "cloudflare-d1",
-      options: {
-        bindingName: "NEWSNOW_DB",
+  } else if (process.env.CF_PAGES) {
+    nitroOption.preset = "cloudflare-pages"
+    nitroOption.unenv = {
+      alias: {
+        "safer-buffer": "node:buffer",
       },
-    },
+    }
+    nitroOption.database = {
+      default: {
+        connector: "cloudflare-d1",
+        options: {
+          bindingName: "NEWSNOW_DB",
+        },
+      },
+    }
+  } else if (process.env.BUN) {
+    nitroOption.preset = "bun"
+    nitroOption.database = {
+      default: {
+        connector: "bun-sqlite",
+      },
+    }
   }
-} else if (process.env.BUN) {
-  nitroOption.preset = "bun"
-  nitroOption.database = {
-    default: {
-      connector: "bun-sqlite",
-    },
-  }
-}
 
-export default function () {
-  return viteNitro(nitroOption)
-}
+  export default function () {
+    return viteNitro(nitroOption)
+  }
